d5010dd7943b48c416efd481a7ea5f193b417b59
Ahn Ming Loke <lokeahnming@gmail.com>
2024-01-08 14:09:18 -0500

    feat: add custom User Model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,20 @@
+"""
+Tests for Models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+
+
+    """Test - successfully creating a user with an email"""
+    def test_create_user_with_email_successful(self):
+        email = "testyTest@example.com"
+        password = "testpassword987"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
